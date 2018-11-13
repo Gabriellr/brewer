b37b45cf5d9f5d2f53075e1bfb216c6b4ff74847
Gabriel <grodrigueschagas@gmail.com>
2018-11-13 15:44:08 -0200

    Hibernate

--- src/main/java/com/projeto/brewer/model/Cerveja.java
+++ src/main/java/com/projeto/brewer/model/Cerveja.java
@@ -1,9 +1,26 @@
 package com.projeto.brewer.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+
+@Entity
+@Table(name = "cerveja")
 public class Cerveja {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
 	
 	
 	@NotBlank(message= "SKU e obrigatorio")
@@ -15,6 +32,18 @@ public class Cerveja {
 	
 	@Size(min = 1, max=50, message = "o tamanho da descricao deve esta entre 1 e 50")
 	private String descricao;
+	
+	
+	private BigDecimal valor;
+	
+	@Column(name = "teor_alcoolico")
+	private BigDecimal teorAcoolico;
+	
+	@Column(name = "quantidade_estoque")
+	private Integer quantidadeEstoque;
+	
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
 	
 	public String getDescricao() {
 		return descricao;
