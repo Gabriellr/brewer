c4eab8582f9cbe9192f74dd8815a46c438b741b6
Gabriel <grodrigueschagas@gmail.com>
2018-11-15 22:56:33 -0200

    Projeto colecao

--- src/main/java/com/projeto/brewer/model/Cerveja.java
+++ src/main/java/com/projeto/brewer/model/Cerveja.java
@@ -9,6 +9,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -39,11 +41,21 @@ public class Cerveja {
 	@Column(name = "teor_alcoolico")
 	private BigDecimal teorAcoolico;
 	
+	private BigDecimal comissao;
+	
 	@Column(name = "quantidade_estoque")
 	private Integer quantidadeEstoque;
 	
 	@Enumerated(EnumType.STRING)
 	private Origem origem;
+	
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_estilo")
+	private Estilo estilo;
+	
 	
 	public String getDescricao() {
 		return descricao;
@@ -63,7 +75,79 @@ public class Cerveja {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public BigDecimal getTeorAcoolico() {
+		return teorAcoolico;
+	}
+	public void setTeorAcoolico(BigDecimal teorAcoolico) {
+		this.teorAcoolico = teorAcoolico;
+	}
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public Origem getOrigem() {
+		return origem;
+	}
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+	public Sabor getSabor() {
+		return sabor;
+	}
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+	public Estilo getEstilo() {
+		return estilo;
+	}
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 		
+	
 	
 
 }
