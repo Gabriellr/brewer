96ecbe0705d411c3efa7dd1187acb633b3014ccf
Gabriel <grodrigueschagas@gmail.com>
2018-11-26 02:32:53 -0200

    configurando 2

--- src/main/java/com/projeto/brewer/controller/CervejasController.java
+++ src/main/java/com/projeto/brewer/controller/CervejasController.java
@@ -7,27 +7,33 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projeto.brewer.model.Cerveja;
+import com.projeto.brewer.model.Origem;
+import com.projeto.brewer.model.Sabor;
 import com.projeto.brewer.repositry.Cervejas;
+import com.projeto.brewer.repositry.Estilos;
 
 @Controller
 public class CervejasController {
 
 	@Autowired
-	private Cervejas cervejas;
-
-
+	private Estilos estilos;
+	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		cervejas.findAll();
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+	 mv.addObject("sabores", Sabor.values());
+	 mv.addObject("estilos", estilos.findAll());
+	 mv.addObject("origens", Origem.values());
 
-		return "cerveja/CadastroCerveja";
+		return mv;
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Validated Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Validated Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()){
 
 			return novo(cerveja);
@@ -35,7 +41,7 @@ public class CervejasController {
 		}
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
 		System.out.println(">>> sku: " + cerveja.getSku());
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 
 }
